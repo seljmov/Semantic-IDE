03dab6038834a0e2b64a4cf3911e700123cf0f45
Zagir Selimov <seljmov@list.ru>
2021-02-06 22:55:40 +0300

    Реализована вкладка 'Работа'

--- Templates/RibbonMenu/RibbonMenu/Program.cs
+++ Templates/RibbonMenu/RibbonMenu/Program.cs
@@ -1,5 +1,7 @@
 using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
+using System;
 
 namespace RibbonMenu
 {
@@ -15,6 +17,7 @@ namespace RibbonMenu
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .LogToTrace()
                 .UseReactiveUI();
     }
 }
--- Templates/RibbonMenu/RibbonMenu/Views/MainWindow.axaml.cs
+++ Templates/RibbonMenu/RibbonMenu/Views/MainWindow.axaml.cs
@@ -9,6 +9,9 @@ namespace RibbonMenu.Views
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
         }
 
         private void InitializeComponent()
